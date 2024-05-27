f2258b77bbfd018b139eb968d3113103c19c969c
Joy <joynandako1@gmail.com>
2024-05-27 13:54:26 +0300

    done

---
+++ lib/class.py
@@ -0,0 +1,14 @@
+
+class Dog:
+    pass
+
+# Creating instances of the Dog class
+fido = Dog()
+snoopy = Dog()
+lassie = Dog()
+
+# Checking if instances are different objects
+print(snoopy == fido) 
+ # False
+print (snoopy == Dog)
+print (snoopy == snoopy)
